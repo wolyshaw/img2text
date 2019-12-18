8b6ad1786d43abf5385ef8ccb33f77716e49c2fb
wolyshaw <wolyshaw@users.noreply.github.com>
2019-12-18 13:30:58 +0800

    add md and script

---
+++ README.md
@@ -0,0 +1,17 @@
+### img  to text
+
+##### 添加配置
+
+```
+cp config.default.js config.js
+```
+
+##### 启动项目
+```
+npm start
+```
+
+##### 浏览器打开
+```
+http://localhost:3000
+```
---
+++ config.default.js
@@ -0,0 +1,13 @@
+module.exports = {
+  oss: {
+    region: '',
+    accessKeyId: '',
+    accessKeySecret: '',
+    bucket: '',
+  },
+  baiduai: {
+    APP_ID: '',
+    API_KEY: '',
+    SECRET_KEY: ''
+  }
+}
